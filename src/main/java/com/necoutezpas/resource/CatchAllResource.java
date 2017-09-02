b494af71701f00f48b0c81988ef60ec954a752be
Pascal LE LEVIER <pascal.lelevier@gmail.com>
2017-09-02 17:44:30 +0200

    Implement CatchAll log forwarding to sse resource

--- src/main/java/com/necoutezpas/resource/CatchAllResource.java
+++ src/main/java/com/necoutezpas/resource/CatchAllResource.java
@@ -1,29 +1,57 @@
 package com.necoutezpas.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
+import org.glassfish.jersey.media.sse.OutboundEvent;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import java.util.Map;
 
 @Path("/")
 public class CatchAllResource {
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     @GET
     @Path("{seg:.*}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response log(@Context UriInfo uriInfo, @Context HttpHeaders headers, @Context HttpServletRequest request) {
-        request.getProtocol();
-        return Response.ok().entity(ImmutableMap.of(
-            "protocol", request.getProtocol(),
-            "method", request.getMethod(),
-            "path", uriInfo.getPath(),
-            "headers", headers.getRequestHeaders()
-        )).build();
+    public Response getLog(@Context UriInfo uriInfo, @Context HttpHeaders headers, @Context HttpServletRequest request)
+            throws JsonProcessingException {
+        Map<String,Object> result = ImmutableMap.of(
+                "protocol", request.getProtocol(),
+                "method", request.getMethod(),
+                "path", uriInfo.getPath(),
+                "headers", headers.getRequestHeaders()
+        );
+        SseResource.write(new OutboundEvent.Builder()
+                .name("log")
+                .data(String.class, objectMapper.writeValueAsString(result))
+                .build());
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("{seg:.*}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postLog(@Context UriInfo uriInfo, @Context HttpHeaders headers, @Context HttpServletRequest request,
+                        String body)
+            throws JsonProcessingException {
+        Map<String,Object> result = ImmutableMap.of(
+                "protocol", request.getProtocol(),
+                "method", request.getMethod(),
+                "path", uriInfo.getPath(),
+                "headers", headers.getRequestHeaders(),
+                "body", body
+        );
+        SseResource.write(new OutboundEvent.Builder()
+                .name("log")
+                .data(String.class, objectMapper.writeValueAsString(result))
+                .build());
+        return Response.ok().build();
     }
 
 }
--- src/main/java/com/necoutezpas/resource/SseResource.java
+++ src/main/java/com/necoutezpas/resource/SseResource.java
@@ -1,6 +1,5 @@
 package com.necoutezpas.resource;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.glassfish.jersey.media.sse.EventOutput;
 import org.glassfish.jersey.media.sse.OutboundEvent;
 import org.glassfish.jersey.media.sse.SseFeature;
@@ -9,71 +8,41 @@ import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 @Path("/sse")
 public class SseResource {
-    private static ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private static ExecutorService executorService = Executors.newFixedThreadPool(20);
+    private static ConcurrentMap<EventOutput,Boolean> watchers = new ConcurrentHashMap<>();
     private static Logger logger = LoggerFactory.getLogger(SseResource.class);
-    private static ObjectMapper mapper = new ObjectMapper();
-    private static volatile EventOutput eventOutput = new EventOutput();
-
-    @GET
-    @Produces(SseFeature.SERVER_SENT_EVENTS)
-    public EventOutput getMessageQueue() {
-        return eventOutput;
-    }
 
     @POST
-    public void addMessage(final String message) throws IOException {
-        final EventOutput localOutput = eventOutput;
-        if (localOutput != null) {
-            eventOutput.write(new OutboundEvent.Builder().name("custom-message").data(String.class, message).build());
-        }
-    }
-
-    @DELETE
-    public void close() throws IOException {
-        final EventOutput localOutput = eventOutput;
-        if (localOutput != null) {
-            eventOutput.close();
-        }
-        setEventOutput(new EventOutput());
-    }
-
-    @POST
-    @Path("domains/{id}")
+    @Path("watch")
     @Produces(SseFeature.SERVER_SENT_EVENTS)
-    public EventOutput startDomain(@PathParam("id") final String id) {
-        final EventOutput seq = new EventOutput();
+    public EventOutput watch() {
+        final EventOutput watcher = new EventOutput();
+        watchers.put(watcher, true);
+        return watcher;
+    }
 
+    public static void write(OutboundEvent event) {
         executorService.submit(() -> {
-            try {
-                seq.write(new OutboundEvent.Builder().name("domain-progress")
-                        .data(String.class, mapper.writeValueAsString("starting domain " + id + " ...")).build());
-                Thread.sleep(200);
-                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("50%")).build());
-                Thread.sleep(200);
-                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("60%")).build());
-                Thread.sleep(200);
-                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("70%")).build());
-                Thread.sleep(200);
-                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("99%")).build());
-                Thread.sleep(200);
-                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("done")).build());
-                seq.close();
-
-            } catch (final InterruptedException | IOException e) {
-                logger.warn(e.getMessage(), e);
+            for (EventOutput watcher : watchers.keySet()) {
+                try {
+                    watcher.write(event);
+                } catch (Exception e) {
+                    logger.warn(e.getMessage(), e);
+                    watchers.remove(watcher);
+                    try {
+                        watcher.close();
+                    } catch (IOException e1) {
+                        logger.warn(e1.getMessage(), e);
+                    }
+                }
             }
         });
-
-        return seq;
-    }
-
-    private static void setEventOutput(final EventOutput eventOutput) {
-        SseResource.eventOutput = eventOutput;
     }
 }
