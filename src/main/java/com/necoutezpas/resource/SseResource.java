ce6f1b370cc0d90dc23afbe140175c92eba40068
Pascal LE LEVIER <pascal.lelevier@gmail.com>
2017-09-02 17:07:59 +0200

    Limit number of threads with executor service

--- src/main/java/com/necoutezpas/resource/SseResource.java
+++ src/main/java/com/necoutezpas/resource/SseResource.java
@@ -1,16 +1,22 @@
 package com.necoutezpas.resource;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.glassfish.jersey.media.sse.EventOutput;
 import org.glassfish.jersey.media.sse.OutboundEvent;
 import org.glassfish.jersey.media.sse.SseFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Path("/sse")
 public class SseResource {
+    private static ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private static Logger logger = LoggerFactory.getLogger(SseResource.class);
     private static ObjectMapper mapper = new ObjectMapper();
     private static volatile EventOutput eventOutput = new EventOutput();
 
@@ -43,28 +49,26 @@ public class SseResource {
     public EventOutput startDomain(@PathParam("id") final String id) {
         final EventOutput seq = new EventOutput();
 
-        new Thread() {
-            public void run() {
-                try {
-                    seq.write(new OutboundEvent.Builder().name("domain-progress")
-                            .data(String.class, mapper.writeValueAsString("starting domain " + id + " ...")).build());
-                    Thread.sleep(200);
-                    seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("50%")).build());
-                    Thread.sleep(200);
-                    seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("60%")).build());
-                    Thread.sleep(200);
-                    seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("70%")).build());
-                    Thread.sleep(200);
-                    seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("99%")).build());
-                    Thread.sleep(200);
-                    seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("done")).build());
-                    seq.close();
+        executorService.submit(() -> {
+            try {
+                seq.write(new OutboundEvent.Builder().name("domain-progress")
+                        .data(String.class, mapper.writeValueAsString("starting domain " + id + " ...")).build());
+                Thread.sleep(200);
+                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("50%")).build());
+                Thread.sleep(200);
+                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("60%")).build());
+                Thread.sleep(200);
+                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("70%")).build());
+                Thread.sleep(200);
+                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("99%")).build());
+                Thread.sleep(200);
+                seq.write(new OutboundEvent.Builder().name("domain-progress").data(String.class, mapper.writeValueAsString("done")).build());
+                seq.close();
 
-                } catch (final InterruptedException | IOException e) {
-                    e.printStackTrace();
-                }
+            } catch (final InterruptedException | IOException e) {
+                logger.warn(e.getMessage(), e);
             }
-        }.start();
+        });
 
         return seq;
     }
